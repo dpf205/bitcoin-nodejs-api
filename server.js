69b38598440159dde59c15373e5706a2c1094415
dpf205 <dpf205@gmail.com>
2017-09-02 18:44:30 -0400

    refactoring

---
+++ server.js
@@ -0,0 +1,17 @@
+var http  = require('http');
+var request =  require('request');
+
+var port = process.env.PORT || 3000;
+
+http.createServer(function (req, res){
+
+   request({
+	   url:"https://blockchain.info/stats?format=json",
+	   json: true
+   }, function(error, response, body){
+	   console.log(body.market_price_usd);
+   });
+	console.log("this is a new bitcoin user" + req.url);
+	res.end("more bitcoin stuff");
+
+}).listen(port);
