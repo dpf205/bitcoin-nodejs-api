115bd805c9685f461c6d81b3c91c2c3e9e72e7f2
dpf205 <dpf205@gmail.com>
2017-09-04 15:29:42 -0400

    added Bootstrap styling and ejs files to /view

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var app =  express();
+var app = express();
 var bodyParser = require('body-parser');
-var request =  require('request');
-var bitcore =  require('bitcore-lib');
+var request = require('request');
+var bitcore = require('bitcore-lib');
 
-var port =  process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({
 	extended: true
@@ -15,30 +15,36 @@ app.use(bodyParser.json());
 app.set("view engine", "ejs");
 
 
-app.get('/', function(req,res){
+app.get('/', function(req, res) {
 
 	request({
- 	   url:"https://blockchain.info/ticker?format=json",
- 	   json: true
-    }, function(error, response, body){
+		url: "https://blockchain.info/ticker?format=json",
+		json: true
+	}, function(error, response, body) {
 
 		var buyPrice = body.USD.buy;
 		var sellPrice = body.USD.sell;
 		var spreadDiff = buyPrice - sellPrice;
 		var spread = spreadDiff.toFixed(2);
 
-		res.render('index', {last_price: body.USD.last, bid_price: body.USD.buy, ask_price: body.USD.sell, spread: spread});
+		res.render('index', {
+			pageTitle: "BTC Convert",
+			last_price: body.USD.last,
+			bid_price: body.USD.buy,
+			ask_price: body.USD.sell,
+			spread: spread
+		});
 
-    });
+	});
 
 
 });
 
-app.post('/create-bitcore-wallet', function(req,res){
+app.post('/create-bitcore-wallet', function(req, res) {
 
 	// console.log(brainsrc);
 
-	var brainsrc =  req.body.brainsrc;
+	var brainsrc = req.body.brainsrc;
 
 	var input = new Buffer(brainsrc);
 
@@ -52,22 +58,22 @@ app.post('/create-bitcore-wallet', function(req,res){
 		address: address,
 		url: `https://blockchain.info/address/${address}?format=json`,
 		json: true
-	}, function(error, response, body){
+	}, function(error, response, body) {
 
 		// console.log(body)
 		res.send('This is the brain wallet of ' + '<b>' + brainsrc + '</b>' +
-		'<br>Address: '  + address +
-		'<br>Private Key Hash: ' + privateKey +
-		'<br> Total Received: ' + body.total_received +
-		'<br> Total Sent: ' + body.total_sent +
-		'<br> Number of Transactions: ' + body.n_tx +
-		'<br> Final Balance: ' + body.final_balance
+			'<br>Address: ' + address +
+			'<br>Private Key Hash: ' + privateKey +
+			'<br> Total Received: ' + body.total_received +
+			'<br> Total Sent: ' + body.total_sent +
+			'<br> Number of Transactions: ' + body.n_tx +
+			'<br> Final Balance: ' + body.final_balance
 		);
 	})
 
 
 });
 
-app.listen(port, function(){
+app.listen(port, function() {
 	console.log(`\n express server on port ${port}`);
 });
